6195bfad585e7ff41b96eea73fa55123fd646760
Spike Storm <software.j2ee@gmail.com>
2020-10-04 22:13:09 -0700

    step one almost done

--- src/main/java/tech/geekflu/bakery/controller/BakeryController.java
+++ src/main/java/tech/geekflu/bakery/controller/BakeryController.java
@@ -4,18 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Map;
 
 @Controller
-//@RequestMapping("/bakery")
+@RequestMapping("/bakery")
 public class BakeryController {
 
   @GetMapping(value = {"/", "/index"})
   public String home(Model model) {
     model.addAttribute("title", "Welcome Bakery!!");
-    return "index";
+    return "shop";
   }
 
 }
