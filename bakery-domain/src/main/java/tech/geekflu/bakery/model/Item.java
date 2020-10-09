4dd8f078a9e681add3be5ee3d677b9e9105d3741
Spike Storm <software.j2ee@gmail.com>
2020-10-08 21:22:48 -0700

    Multi projerct configuration, tests were lost

--- bakery-domain/src/main/java/tech/geekflu/bakery/model/Item.java
+++ bakery-domain/src/main/java/tech/geekflu/bakery/model/Item.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 @Setter
 @Builder
 public class Item {
+
   private Long id;
   private String name;
   private String description;
@@ -15,4 +16,6 @@ public class Item {
   private String image;
   private boolean isActive;
   private Category category;
+
+
 }
