b387898e4e685a7af0d082b9e6f5a38375680d86
Spike Storm <software.j2ee@gmail.com>
2020-10-23 12:25:30 -0700

    test configuration working

--- bakery-boot-app/src/test/groovy/tech/geekflu/bakery/BakeryControllerSpec.groovy
+++ bakery-boot-app/src/test/groovy/tech/geekflu/bakery/BakeryControllerSpec.groovy
@@ -1,25 +1,30 @@
 package tech.geekflu.bakery
 
+import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.http.MediaType
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
 import tech.geekflu.bakery.application.BakeryApplicationService
-import tech.geekflu.bakery.controller.BakeryController
 import tech.geekflu.bakery.domain.model.CatalogInformation
 import tech.geekflu.bakery.domain.model.Category
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@WebMvcTest(controllers = [BakeryController.class])
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class BakeryControllerSpec extends BasicSpecification {
+
 
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    private BakeryApplicationService applicationService;
+    private BakeryApplicationService applicationService
 
     def "Bakery Home Success"(){
 
@@ -60,7 +65,7 @@ class BakeryControllerSpec extends BasicSpecification {
         ]
 
         when:
-        def result = mockMvc.perform(get("/bakery").accept(MediaType.TEXT_HTML))
+        ResultActions result = mockMvc.perform(get("/bakery/"))
 
         then:
         result.andExpect(status().isOk())
